3333478e760ee3fb12f96a6b9c0b34563f2d5f09
Peter Preeper <ppreeper@gmail.com>
2023-06-21 14:15:32 -0600

    [imp] add db_copy

---
+++ cmd/db_copy/database/routines.go
@@ -0,0 +1,104 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+//////////
+// Routines
+//////////
+
+// Routine list of routines (procedures, functions)
+type Routine struct {
+	Name             string `db:"ROUTINE_NAME"`
+	Type             string `db:"ROUTINE_TYPE"`
+	Definition       string `db:"ROUTINE_DEFINITION"`
+	DataType         string `db:"DATA_TYPE"`
+	ExternalLanguage string `db:"EXTERNAL_LANGUAGE"`
+}
+
+type RoutineList struct {
+	Name string `db:"ROUTINE_NAME"`
+}
+
+// GetRoutines returns list of routines and definitions
+func (db *Database) GetRoutines(schema string, timeout int) ([]RoutineList, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer cancel()
+	q := ""
+	if db.Driver == "postgres" || db.Driver == "mssql" {
+		q += "SELECT ROUTINE_NAME \"ROUTINE_NAME\"" + "\n"
+		q += "FROM INFORMATION_SCHEMA.ROUTINES" + "\n"
+		q += "WHERE ROUTINE_SCHEMA = '" + schema + "'" + "\n"
+		q += "AND ROUTINE_DEFINITION IS NOT NULL" + "\n"
+		q += "ORDER BY ROUTINE_NAME" + "\n"
+	}
+	rr := []RoutineList{}
+	if err := db.SelectContext(ctx, &rr, q); err != nil {
+		return nil, fmt.Errorf("select: %w", err)
+	}
+	return rr, nil
+}
+
+// GetRoutineSchema returns routine and definition
+func (db *Database) GetRoutineSchema(schema, routine string) (Routine, error) {
+	q := ""
+	if db.Driver == "postgres" || db.Driver == "mssql" {
+		q += "SELECT ROUTINE_NAME \"ROUTINE_NAME\"" + "\n"
+		q += ",ROUTINE_TYPE \"ROUTINE_TYPE\"" + "\n"
+		q += ",ROUTINE_DEFINITION \"ROUTINE_DEFINITION\"" + "\n"
+		q += ",CASE WHEN DATA_TYPE IS NULL THEN '' ELSE DATA_TYPE END \"DATA_TYPE\"" + "\n"
+		q += ",CASE WHEN EXTERNAL_LANGUAGE IS NULL THEN '' ELSE EXTERNAL_LANGUAGE END \"EXTERNAL_LANGUAGE\"" + "\n"
+		q += "FROM INFORMATION_SCHEMA.ROUTINES" + "\n"
+		q += "WHERE ROUTINE_SCHEMA = '" + schema + "'" + "\n"
+		q += "AND ROUTINE_NAME = '" + routine + "'" + "\n"
+		q += "AND ROUTINE_DEFINITION IS NOT NULL" + "\n"
+		q += "ORDER BY ROUTINE_NAME" + "\n"
+	}
+	rr := Routine{}
+	if err := db.Get(&rr, q); err != nil {
+		return Routine{}, fmt.Errorf("select: %w", err)
+	}
+	return rr, nil
+}
+
+// GetRoutine gets procedure definition
+func (db *Database) GetRoutine(d Database, schema string, r Routine, dbg bool) {
+	fmt.Printf("\n-- ROUTINE: %s.%s", schema, r.Name)
+	q := ""
+	if d.Driver == "postgres" {
+		if r.Type == "PROCEDURE" {
+			q += "DROP " + r.Type + " IF EXISTS " + schema + "." + r.Name + "();\n"
+			q += "CREATE OR REPLACE " + r.Type + " " + schema + "." + r.Name + "()\n"
+			q += "LANGUAGE sql\n"
+			q += "AS $procedure$"
+			q += r.Definition
+			q += "$procedure$\n;"
+		} else if r.Type == "FUNCTION" {
+			q += r.Definition
+		}
+	} else if d.Driver == "mssql" {
+		if r.Type == "PROCEDURE" {
+			q += "DROP " + r.Type + " " + schema + "." + r.Name + ";\n"
+			q += r.Definition + "\n"
+		} else if r.Type == "FUNCTION" {
+			q += "DROP " + r.Type + " " + schema + "." + r.Name + ";\n"
+			q += r.Definition
+		}
+	}
+
+	if dbg {
+		fmt.Printf("\n%v\n", q)
+	} else {
+		t := strings.Replace(r.Name, "/", "_", -1)
+		fname := fmt.Sprintf("%s.%s.%s.%s.sql", d.Database, schema, t, r.Type)
+		f, err := os.Create(fname)
+		db.checkErr(err)
+		defer f.Close()
+		f.Write([]byte(q))
+	}
+}
---
+++ cmd/db_copy/database/tableColumns.go
@@ -0,0 +1,144 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+//Column struct
+type Column struct {
+	Column     string `db:"CL"`
+	ColumnName string `db:"CN"`
+	DataType   string `db:"DT"`
+}
+
+//GetColumnDetail func
+func (db *Database) GetColumnDetail(conn *Conn, t string, debug bool, timeout int) ([]Column, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer cancel()
+	q := ""
+	if conn.Source.Driver == "mssql" {
+		if conn.Dest.Driver == "mssql" {
+			q += "-- mssql to mssql\n"
+			q += "SELECT\n"
+			q += "'\"' + C.COLUMN_NAME + '\" ' +\n"
+			q += "CASE UPPER(DATA_TYPE)\n"
+			q += "WHEN 'CHAR' THEN 'CHAR' + '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')'\n"
+			q += "WHEN 'NCHAR' THEN 'CHAR' + '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')'\n"
+			q += "WHEN 'VARCHAR' THEN CASE WHEN C.CHARACTER_MAXIMUM_LENGTH < 0 then 'TEXT' ELSE 'VARCHAR' + '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')' END\n"
+			q += "WHEN 'NVARCHAR' THEN CASE WHEN C.CHARACTER_MAXIMUM_LENGTH < 0 then 'TEXT' ELSE 'VARCHAR'+ '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')' END\n"
+			q += "WHEN 'CHARACTER' THEN 'CHARACTER' + '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')'\n"
+			q += "WHEN 'CHARACTER VARYING' THEN CASE WHEN C.CHARACTER_MAXIMUM_LENGTH < 0 then 'TEXT' ELSE 'CHARACTER VARYING'+ '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')' END\n"
+			q += "WHEN 'TINYINT' THEN 'TINYINT'\n"
+			q += "WHEN 'SMALLINT' THEN 'SMALLINT'\n"
+			q += "WHEN 'INT' THEN 'INT'\n"
+			q += "WHEN 'DECIMAL' THEN 'DECIMAL' + '(' + CONVERT(VARCHAR,C.NUMERIC_PRECISION) + ',' + CONVERT(VARCHAR,C.NUMERIC_SCALE) + ')'\n"
+			q += "WHEN 'NUMERIC' THEN 'NUMERIC' + '(' + CONVERT(VARCHAR,C.NUMERIC_PRECISION) + ',' + CONVERT(VARCHAR,C.NUMERIC_SCALE) + ')'\n"
+			q += "WHEN 'FLOAT' THEN 'FLOAT' + CASE WHEN C.NUMERIC_PRECISION < 53 THEN '(' + CONVERT(VARCHAR,C.NUMERIC_PRECISION) + ')' ELSE '' END\n"
+			q += "WHEN 'VARBINARY' THEN 'VARBINARY'\n"
+			q += "WHEN 'DATETIME' THEN 'DATETIME'\n"
+			q += "ELSE DATA_TYPE\n"
+			q += "END + ' ' +\n"
+			q += "CASE WHEN IS_NULLABLE = 'NO' THEN 'NOT NULL' ELSE '' END + ' ' +\n"
+			q += "CASE WHEN C.COLUMN_DEFAULT IS NULL THEN ''\n"
+			q += "ELSE ' DEFAULT ' + SUBSTRING(C.COLUMN_DEFAULT,CHARINDEX(' as ', C.COLUMN_DEFAULT)+4,LEN(C.COLUMN_DEFAULT)-CHARINDEX(' as ', C.COLUMN_DEFAULT)) END\n"
+			q += "\"CL\", C.COLUMN_NAME \"CN\", UPPER(DATA_TYPE) \"DT\"\n"
+		} else if conn.Dest.Driver == "postgres" {
+			q += "-- mssql to pgsql\n"
+			q += "SELECT\n"
+			q += "'\"' + C.COLUMN_NAME + '\" ' +\n"
+			q += "CASE UPPER(DATA_TYPE)\n"
+			q += "WHEN 'CHAR' THEN 'CHARACTER' + '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')'\n"
+			q += "WHEN 'NCHAR' THEN 'CHARACTER' + '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')'\n"
+			q += "WHEN 'VARCHAR' THEN CASE WHEN C.CHARACTER_MAXIMUM_LENGTH < 0 then 'TEXT' ELSE 'CHARACTER VARYING' + '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')' END\n"
+			q += "WHEN 'NVARCHAR' THEN CASE WHEN C.CHARACTER_MAXIMUM_LENGTH < 0 then 'TEXT' ELSE 'CHARACTER VARYING'+ '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')' END\n"
+			q += "WHEN 'CHARACTER' THEN 'CHARACTER' + '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH) + ')'\n"
+			q += "WHEN 'CHARACTER VARYING' THEN CASE WHEN C.CHARACTER_MAXIMUM_LENGTH < 0 then 'TEXT' ELSE 'CHARACTER VARYING'+ '(' + CONVERT(VARCHAR,C.CHARACTER_MAXIMUM_LENGTH)+')' END\n"
+			q += "WHEN 'TINYINT' THEN 'SMALLINT'\n"
+			q += "WHEN 'SMALLINT' THEN 'SMALLINT'\n"
+			q += "WHEN 'INT' THEN 'INT'\n"
+			q += "WHEN 'DECIMAL' THEN 'NUMERIC' + '(' + CONVERT(VARCHAR,C.NUMERIC_PRECISION) + ',' + CONVERT(VARCHAR,C.NUMERIC_SCALE) + ')'\n"
+			q += "WHEN 'NUMERIC' THEN 'NUMERIC' + '(' + CONVERT(VARCHAR,C.NUMERIC_PRECISION) + ',' + CONVERT(VARCHAR,C.NUMERIC_SCALE) + ')'\n"
+			q += "WHEN 'FLOAT' THEN 'DOUBLE PRECISION' + CASE WHEN C.NUMERIC_PRECISION < 53 THEN '(' + CONVERT(VARCHAR,C.NUMERIC_PRECISION) + ')' ELSE '' END\n"
+			q += "WHEN 'DOUBLE PRECISION' THEN 'DOUBLE PRECISION' + CASE WHEN C.NUMERIC_PRECISION < 53 THEN '(' + CONVERT(VARCHAR,C.NUMERIC_PRECISION) + ')' ELSE '' END\n"
+			q += "WHEN 'VARBINARY' THEN 'BYTEA'\n"
+			q += "WHEN 'DATETIME' THEN 'TIMESTAMP'\n"
+			q += "ELSE DATA_TYPE\n"
+			q += "END + ' ' +\n"
+			q += "CASE WHEN IS_NULLABLE = 'NO' THEN 'NOT NULL' ELSE '' END + ' ' +\n"
+			q += "CASE WHEN C.COLUMN_DEFAULT IS NULL THEN ''\n"
+			q += "ELSE ' DEFAULT ' + substring(C.COLUMN_DEFAULT,CASE WHEN CHARINDEX(' as ', C.COLUMN_DEFAULT) = 0 then 0 else CHARINDEX(' as ', C.COLUMN_DEFAULT)+4 end,LEN(C.COLUMN_DEFAULT)+1-CASE WHEN CHARINDEX(' as ', C.COLUMN_DEFAULT) = 0 then 0 else CHARINDEX(' as ', C.COLUMN_DEFAULT) end) END\n"
+			q += "\"CL\", C.COLUMN_NAME \"CN\", UPPER(DATA_TYPE) \"DT\"\n"
+		}
+	} else if conn.Source.Driver == "postgres" {
+		if conn.Dest.Driver == "mssql" {
+			q += "-- pgsql to mssql\n"
+			q += "SELECT\n"
+			q += "'\"' || C.COLUMN_NAME || '\" ' ||\n"
+			q += "CASE UPPER(DATA_TYPE)\n"
+			q += "WHEN 'CHAR' THEN 'CHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'NCHAR' THEN 'CHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'VARCHAR' THEN 'VARCHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'NVARCHAR' THEN 'VARCHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'CHARACTER' THEN 'CHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'CHARACTER VARYING' THEN 'VARCHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'TINYINT' THEN 'TINYINT'\n"
+			q += "WHEN 'SMALLINT' THEN 'SMALLINT'\n"
+			q += "WHEN 'INT' THEN 'INT'\n"
+			q += "WHEN 'DECIMAL' THEN 'DECIMAL' || case when C.NUMERIC_PRECISION::character varying IS NULL THEN '' ELSE '(' || C.NUMERIC_PRECISION::character varying || ',' || C.NUMERIC_SCALE::character varying || ')' END\n"
+			q += "WHEN 'NUMERIC' THEN 'DECIMAL' || case when C.NUMERIC_PRECISION::character varying IS NULL THEN '' ELSE '(' || C.NUMERIC_PRECISION::character varying || ',' || C.NUMERIC_SCALE::character varying || ')' END\n"
+			q += "WHEN 'FLOAT' THEN 'FLOAT' || CASE WHEN C.NUMERIC_PRECISION < 53 THEN '(' || C.NUMERIC_PRECISION::character varying || ')' ELSE '' end\n"
+			q += "WHEN 'DOUBLE PRECISION' THEN 'FLOAT' || CASE WHEN C.NUMERIC_PRECISION < 53 THEN '(' || C.NUMERIC_PRECISION::character varying || ')' ELSE '' END\n"
+			q += "WHEN 'VARBINARY' THEN 'VARBINARY'\n"
+			q += "WHEN 'BYTEA' THEN 'VARBINARY'\n"
+			q += "WHEN 'DATETIME' THEN 'DATETIME'\n"
+			q += "ELSE DATA_TYPE\n"
+			q += "END || ' ' ||\n"
+			q += "CASE WHEN IS_NULLABLE = 'NO' THEN 'NOT NULL' ELSE '' END || ' ' ||\n"
+			q += "CASE WHEN C.COLUMN_DEFAULT IS NULL THEN ''\n"
+			q += "ELSE ' DEFAULT ' || case when POSITION('::' in C.COLUMN_DEFAULT) > 0 then SUBSTRING(C.COLUMN_DEFAULT,1,POSITION('::' in C.COLUMN_DEFAULT)-1) else C.COLUMN_DEFAULT END end\n"
+			q += "\"CL\", C.COLUMN_NAME \"CN\", UPPER(DATA_TYPE) \"DT\"\n"
+		} else if conn.Dest.Driver == "postgres" {
+			q += "-- pgsql to pgsql\n"
+			q += "SELECT\n"
+			q += "'\"' || C.COLUMN_NAME || '\" ' ||\n"
+			q += "CASE UPPER(DATA_TYPE)\n"
+			q += "WHEN 'CHAR' THEN 'CHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'NCHAR' THEN 'CHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'VARCHAR' THEN 'VARCHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'NVARCHAR' THEN 'VARCHAR' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'CHARACTER' THEN 'CHARACTER' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'CHARACTER VARYING' THEN 'CHARACTER VARYING' || CASE WHEN C.CHARACTER_MAXIMUM_LENGTH::character varying IS NULL THEN '' ELSE '(' || C.CHARACTER_MAXIMUM_LENGTH::character varying || ')' END \n"
+			q += "WHEN 'TINYINT' THEN 'TINYINT'\n"
+			q += "WHEN 'SMALLINT' THEN 'SMALLINT'\n"
+			q += "WHEN 'INT' THEN 'INT'\n"
+			q += "WHEN 'DECIMAL' THEN 'DECIMAL' || case when C.NUMERIC_PRECISION::character varying IS NULL THEN '' ELSE '(' || C.NUMERIC_PRECISION::character varying || ',' || C.NUMERIC_SCALE::character varying || ')' END\n"
+			q += "WHEN 'NUMERIC' THEN 'NUMERIC' || case when C.NUMERIC_PRECISION::character varying IS NULL THEN '' ELSE '(' || C.NUMERIC_PRECISION::character varying || ',' || C.NUMERIC_SCALE::character varying || ')' END\n"
+			q += "WHEN 'FLOAT' THEN 'FLOAT' || CASE WHEN C.NUMERIC_PRECISION < 53 THEN '(' || C.NUMERIC_PRECISION::character varying || ')' ELSE '' END\n"
+			q += "WHEN 'VARBINARY' THEN 'VARBINARY'\n"
+			q += "WHEN 'DATETIME' THEN 'DATETIME'\n"
+			q += "ELSE DATA_TYPE\n"
+			q += "END || ' ' ||\n"
+			q += "CASE WHEN IS_NULLABLE = 'NO' THEN 'NOT NULL' ELSE '' END || ' ' ||\n"
+			q += "CASE WHEN C.COLUMN_DEFAULT IS NULL THEN ''\n"
+			q += "ELSE ' DEFAULT ' || C.COLUMN_DEFAULT END\n"
+			q += "\"CL\", C.COLUMN_NAME \"CN\", UPPER(DATA_TYPE) \"DT\"\n"
+		}
+	}
+
+	q += fmt.Sprintf("FROM %s.INFORMATION_SCHEMA.COLUMNS C\n", conn.Source.Database)
+	q += fmt.Sprintf("WHERE C.TABLE_CATALOG = '%s'\n", conn.Source.Database)
+	q += fmt.Sprintf("AND C.TABLE_SCHEMA = '%s'\n", conn.SSchema)
+	q += fmt.Sprintf("AND C.TABLE_NAME = '%s'\n", t)
+	q += "ORDER BY ORDINAL_POSITION;\n;"
+
+	if debug {
+		fmt.Println(q)
+	}
+	columnnames := []Column{}
+	if err := db.SelectContext(ctx, &columnnames, q); err != nil {
+		return nil, fmt.Errorf("select: %v", err)
+	}
+	return columnnames, nil
+}
